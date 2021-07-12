1673f0b0f82a2975876829109a32b8df3b8232c9
dewey <dewey.seo@icloud.com>
2021-07-12 15:28:17 +0700

    feat: on developing

--- bean/ViewControllers/Posting/PostingPlaceViewController.swift
+++ bean/ViewControllers/Posting/PostingPlaceViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import MapKit
 
 protocol PostingPlaceViewControllerDelegate: AnyObject {
     func didFinishedPosting()
@@ -14,21 +15,13 @@ protocol PostingPlaceViewControllerDelegate: AnyObject {
 
 class PostingPlaceViewController: UIViewController {
     weak var delegate: PostingPlaceViewControllerDelegate?
+    
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
